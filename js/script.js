9fa6e4a3cee7a50c7a63f68955c20679bd6a8291
MateusCastro <mateus.castro.domingos@gmail.com>
2019-05-26 22:40:19 -0300

    Site finalizado

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function () {
+    setInterval(() => {
+        var hour = new Date().getHours();
+        var aberto = document.getElementsByClassName("aberto");
+        if(hour > 7 && hour < 18){
+            aberto[0].style.background = "green";
+        }else{
+            aberto[0].style.background = "red";
+        }
+        console.log(hour);
+    }, 1000)
+})();
